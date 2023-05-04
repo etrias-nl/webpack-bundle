6fb9d5bc685331a86679cfab611e7e6aa88677b9
Roland Franssen <franssen.roland@gmail.com>
2023-05-04 16:28:44 +0200

    minor

--- src/Config/WebpackConfigDumper.php
+++ src/Config/WebpackConfigDumper.php
@@ -32,18 +32,18 @@ class WebpackConfigDumper
      */
     public function dump(WebpackConfig $config)
     {
-        $configTemplate = 'module.exports = require(%s)(%s);';
+        $configTemplate = 'module.exports = require(__dirname+%s)(%s);';
         $configContents = sprintf(
             $configTemplate,
-            json_encode($this->includeConfigPath, JSON_UNESCAPED_SLASHES),
+            json_encode('/'.basename($this->includeConfigPath), JSON_UNESCAPED_SLASHES),
             json_encode([
                 'entry' => (object)$config->getEntryPoints(),
-                'groups' => (object)$config->getAssetGroups(),
+                //'groups' => (object)$config->getAssetGroups(),
                 'alias' => (object)$config->getAliases(),
-                'manifest_path' => $this->manifestPath,
-                'environment' => $this->environment,
-                'parameters' => (object)$this->parameters,
-            ], JSON_UNESCAPED_SLASHES)
+                //'manifest_path' => $this->manifestPath,
+                //'environment' => $this->environment,
+                //'parameters' => (object)$this->parameters,
+            ], JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT)
         );
 
         file_put_contents($this->path, $configContents);
